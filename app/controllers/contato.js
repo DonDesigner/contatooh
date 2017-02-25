4c90a88864d8559e8647594c5179f2b09de00bb6
lfdf1540 <dondesigner@hotmail.com>
2017-02-24 21:51:47 -0300

    em app, adicionado as rotas e os CRUD

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -3,32 +3,77 @@
 
 module.exports = function (app) {
 
-    var Contato = app.models.contato;
+  var Contato = app.models.contato;
 
-    var controller = {};
+  var controller = {};
 
-    controller.listaContatos = function (req, res) {
-        var promise = Contato.find().exec()
-        .then(
-            function(contatos){
-                res.json(contatos);
-            },
-            function(erro){
-                Console.error(erro);
-                res.status(505).json(erro);
-            }
-        );
-    };
+  controller.listaContatos = function (req, res) {
+    var promise = Contato.find().exec()
+      .then(
+      function (contatos) {
+        res.json(contatos);
+      },
+      function (erro) {
+        Console.error(erro);
+        res.status(505).json(erro);
+      }
+      );
+  };
 
-    controller.obtemContato = function (req, res) {
-    };
+  controller.obtemContato = function (req, res) {
 
-    controller.removeContato = function (req, res) {
-    };
+    var _id = req.params.id;
+    Contato.findById(_id).exec()
+      .then(function (contato) {
+        if (!contato) throw new Error("Contato não encontrado");
+        res.json(contato);
 
-    controller.salvarContato = function (req, res) {
-    };
+      },
+      function (erro) {
+        console.log(erro);
+        res.status(404).json(erro);
+      }
+      )
+  };
+
+  controller.removeContato = function (req, res) {
+    var _id = req.params.id;
+    Contato.remove({ "_id": _id }).exec()
+      .then(function () {
+        res.end();
+      },
+      function (erro) {
+        return console.error(error);
+      }
+      )
+
+  };
+
+  controller.salvarContato = function (req, res) {
+    var _id = req.body._id;
+    if (_id) {
+      Contato.findByIdAndUpdate(_id, req.body).exec()
+        .then(function (contato) {
+          res.json(contato);
+        },
+        function (erro) {
+          console.error(erro)
+          res.status(500).json(erro);
+        });
+    } else {
+      Contato.create(req.body)
+      .then(
+        function(contato){
+          res.status(201).json(contato);
+        },
+        function(erro){
+          console.log(erro);
+          res.status(500).json(erro);
+        }
+      )
+    }
+  };
 
 
-    return controller;
+  return controller;
 }
