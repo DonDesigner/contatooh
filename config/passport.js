e61603628a0ae05ceb6ee3019b00f5f3de658efc
lfdf1540 <dondesigner@hotmail.com>
2017-02-24 23:47:15 -0300

    adicionando e conectando Passport ao mongodb

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,29 @@
 var passport = require('passport');
+var mongoose = require('mongoose');
 var GitHubStrategy = require('passport-github').Strategy;
 
-module.exports = function() {
-    passport.use(new GitHubStrategy({
-        clientID : 'c793712ab8eede556948',
-        clientSecret : '7baa2b44a046c63636b6447a0102abfdf1623570',
-        callbackURL : 'http://localhost:3000/auth/github/callback'
-    }, function(accessToken, refreshToken, profile, done){
+module.exports = function () {
 
-    }
-    ));
+    var Usuario = mongoose.model('Usuario');
+
+    passport.use(new GitHubStrategy({
+        clientID: 'c793712ab8eede556948',
+        clientSecret: '7baa2b44a046c63636b6447a0102abfdf1623570',
+        callbackURL: 'http://localhost:3000/auth/github/callback'
+    }, function (accessToken, refreshToken, profile, done) {
+        Usuario.findOrCreate(
+            { 'login': profile.username },
+            { 'nome': profile.username },
+            function (erro, usuario) {
+                if (erro) {
+                    console.log(erro);
+                    return done(erro);
+                }
+
+                return done(null, usuario)
+            }
+
+
+        );
+    }));
 };
