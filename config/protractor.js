0b544856726a4b10f8542640a88fb1f19be7e68d
Diego Fernandes <dondesigner@hotmail.com>
2017-03-24 16:39:18 -0300

    Update protractor.js
    set error

--- config/protractor.js
+++ config/protractor.js
@@ -4,7 +4,7 @@ exports.config ={
     browser.driver.get('http://localhost:3000');
     browser.driver.findElement(by.id('entrar')).click();
     browser.driver.findElement(by.id('login_field')).sendKeys('dondesigner@hotmail.com');
-    browser.driver.findElement(by.id('password')).sendKeys('lfdf1998');
+    browser.driver.findElement(by.id('password')).sendKeys('*******');
     browser.driver.findElement(by.name('commit')).click();
   }
 };
